7762e46ed04944f70e4ea89e9b85cb37cf664a7b
yuthikasagarage <sandamalsagarage@gmail.com>
2021-10-19 04:28:02 +0530

    all done

--- lib/hello-cdk-stack.ts
+++ lib/hello-cdk-stack.ts
@@ -1,11 +1,9 @@
-import * as sns from '@aws-cdk/aws-sns';
-import * as subs from '@aws-cdk/aws-sns-subscriptions';
-import * as sqs from '@aws-cdk/aws-sqs';
 import * as cdk from '@aws-cdk/core';
 import * as lambda from '@aws-cdk/aws-lambda';
-import * as iam from '@aws-cdk/aws-iam';
-import * as s3 from '@aws-cdk/aws-s3';
 import * as apigw from '@aws-cdk/aws-apigateway';
+import { IResource, LambdaIntegration, MockIntegration, PassthroughBehavior, RestApi } from '@aws-cdk/aws-apigateway';
+import { Resource } from '@aws-cdk/core';
+
 export class HelloCdkStack extends cdk.Stack {
   constructor(scope: cdk.App, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
@@ -18,19 +16,34 @@ export class HelloCdkStack extends cdk.Stack {
       },
     });
     
-    const api = new apigw.RestApi(this, 'HelloCdkApi');
+    const api = new apigw.RestApi(this, 'HelloCdkApi', {
+      defaultCorsPreflightOptions: {
+        allowOrigins: apigw.Cors.ALL_ORIGINS
+      }
+    });
 
     const integration = new apigw.LambdaIntegration(lambdaFunction);
 
     api.root.addMethod('ANY', integration);
 
-    api.root.resourceForPath('/')
+    
+
+    api.root.resourceForPath('/acquire')
+      .addMethod('POST', new apigw.LambdaIntegration(lambdaFunction));
+
+    
+    api.root.resourceForPath('/start')
+      .addMethod('POST', new apigw.LambdaIntegration(lambdaFunction));
+    
+
+    api.root.resourceForPath('/stop')
       .addMethod('POST', new apigw.LambdaIntegration(lambdaFunction));
 
     
     new cdk.CfnOutput(this, 'ApiUrl', {
       value: api.url ?? "something went wrong ",
     });
+
 
   }
 
